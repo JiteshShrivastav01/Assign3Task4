6b7fa4077c1056778e79b5401272549b601f8d3a
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-06-07 23:31:00 +0530

    Assign3Task6

--- src/Components/List/AddUser.js
+++ src/Components/List/AddUser.js
@@ -2,21 +2,28 @@ import { useState } from "react"
 import Button from "../UI/Button"
 import './AddUser.css'
 import Card from '../UI/Card'
+import { useRef } from "react"
 
 
 
 const AddUser=()=>{
     const [User,setUser]=useState([])
-    const [Username,setUsername]=useState('')
-    const [Age,setAge]=useState('')
+    const usernameInputRef=useRef('')
+    const ageInputRef=useRef('')
+    const collegeInputRef=useRef('')
+    //const [Username,setUsername]=useState('')
+    //const [Age,setAge]=useState('')
     const [Error,setError]=useState('')
     const [isValid,setIsValid]=useState(true)
 
     const submitHandler=(event)=>{
         event.preventDefault();
-        const newUser={Username,Age}
+        const Username=usernameInputRef.current.value;
+        const Age=ageInputRef.current.value;
+        const College=collegeInputRef.current.value;
+        const newUser={Username,Age,College}
         
-        if(Username.trim().length === 0 && Age.trim().length === 0){
+        if(Username.trim().length === 0 && Age.trim().length === 0 && College.trim().length===0){
           setError('Please fill the form.')
           setIsValid(false)
           return;
@@ -35,12 +42,20 @@ const AddUser=()=>{
           setError('Please enter Age.')
           setIsValid(false)
           return;
-      }
+        }
+        else if (College.trim().length===0){
+          setError('Enter College Name.')
+          setIsValid(false)
+          return;
+        }
         else{
           setUser([...User,newUser])
         }
-        setUsername('')
-        setAge('')
+        ageInputRef.current.value=''
+        usernameInputRef.current.value=''
+        collegeInputRef.current.value=''
+        //setUsername('')
+        //setAge('')
     }
     
     
@@ -48,17 +63,25 @@ const AddUser=()=>{
     const errorOkay=(e)=>{
       e.preventDefault();
       setIsValid(true)
-      setAge('')
-      setUsername('')
+      ageInputRef.current.value=''
+      usernameInputRef.current.value=''
+      collegeInputRef.current.value=''
+      //setAge('')
+      //setUsername('')
     }
 
     const usernameChange=(event)=>{
       event.preventDefault();
-      setUsername(event.target.value);
+      //setUsername(event.target.value);
+      setIsValid(true)
+    }
+    const collegeChange=(event)=>{
+      event.preventDefault();
+      //setUsername(event.target.value);
       setIsValid(true)
     }
     const ageChange=(event)=>{
-      setAge(event.target.value);
+      //setAge(event.target.value);
       setIsValid(true)
     }
 
@@ -67,9 +90,13 @@ const AddUser=()=>{
       <form onSubmit={submitHandler} className="formControl">
         <p>New User form</p><hr />
         <label htmlFor="Username">Username :</label>
-        <input type="text" value={Username} onChange={usernameChange} /><br /><br />
+        <input type="text" name='usernameRef' onChange={usernameChange} ref={usernameInputRef}/>
+        {/* <input type="text" value={Username} onChange={usernameChange} /><br /><br /> */}
         <label htmlFor="age">Age :</label>
-        <input type="number" value={Age} onChange={ageChange} /> <br />
+        <input type="number" name="ageRef" onChange={ageChange} ref={ageInputRef} />
+        {/* <input type="number" value={Age} onChange={ageChange} /> <br /> */}
+        <label htmlFor="Username">College Name :</label>
+        <input type="text" name='collegeRef' onChange={collegeChange} ref={collegeInputRef}/>
         <Button type="submit" >Add User</Button>
       </form>
 
@@ -91,7 +118,7 @@ const AddUser=()=>{
       <ul className="listBox">
         {
           User.map((user,index)=>(
-            <li className="list" key={index}>{user.Username}({user.Age} years old.)</li>
+            <li className="list" key={index}>{user.Username}( {user.Age} years old ) from {user.College}</li>
           ))
         }
       </ul><hr />
